1d96cfbee981ce5f6406242e265c490e5dc5cc36
Dominic Frei <business.dominic.frei@gmail.com>
2020-10-23 21:19:21 +0100

    Camera movement.

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -1,14 +1,58 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class MainCamera : MonoBehaviour
 {
-    public void ZoomIn()
-    {
+    [SerializeField] private Player player;
 
+    private Vector3 mapOverviewPosition = new Vector3(5.0f, 52.0f, 0.0f);
+    private Vector3 playerOffset = new Vector3(0.0f, 15.0f, 0.0f);
+    private String zoomInText = "Zoom In";
+    private String zoomOutText = "Zoom Out";
+
+    enum Focus
+    {
+        Map,
+        Room,
+        Player
+    }
+    private Focus focus = Focus.Player;
+
+    // Gets called when the zoom button is pressed.
+    // Returns the text that the zoom button should show next.
+    public String ToggleZoom()
+    {
+        String newButtonText = String.Empty;
+
+        switch (focus)
+        {
+            case Focus.Player:
+                focus = Focus.Map;
+                newButtonText = zoomInText;
+                break;
+            case Focus.Map:
+                focus = Focus.Player;
+                newButtonText = zoomOutText;
+                break;
+            case Focus.Room:
+                break;
+        }
+
+        return newButtonText;
     }
 
-    public void ZoomOut()
+    private void Update()
     {
-
+        switch (focus)
+        {
+            case Focus.Player:
+                transform.position = player.transform.position + playerOffset;
+                break;
+            case Focus.Map:
+                transform.position = mapOverviewPosition;
+                break;
+            case Focus.Room:
+                break;
+        }
     }
 }
--- Assets/Scripts/ZoomButton.cs
+++ Assets/Scripts/ZoomButton.cs
@@ -5,28 +5,10 @@ public class ZoomButton : MonoBehaviour
 {
     [SerializeField] MainCamera mainCamera = default;
 
-    enum ZoomState
-    {
-        In,
-        Out
-    }
-    ZoomState zoomState = ZoomState.In;
-
     public void ZoomButtonClicked()
     {
-        switch (zoomState)
-        {
-            case ZoomState.In:
-                zoomState = ZoomState.Out;
-                mainCamera.ZoomOut();
-                GetComponentInChildren<Text>().text = "Zoom In";
-                break;
-            case ZoomState.Out:
-                zoomState = ZoomState.In;
-                mainCamera.ZoomIn();
-                GetComponentInChildren<Text>().text = "Zoom Out";
-                break;
-        }
+        string newText = mainCamera.ToggleZoom();
+        GetComponentInChildren<Text>().text = newText;
     }
 
 }
