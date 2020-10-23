1602d299d0768556b66c7af5864e4078df275573
Dominic Frei <business.dominic.frei@gmail.com>
2020-10-23 21:04:08 +0100

    Add zoom button.

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public void ZoomIn()
+    {
+
+    }
+
+    public void ZoomOut()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ZoomButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ZoomButton : MonoBehaviour
+{
+    [SerializeField] MainCamera mainCamera = default;
+
+    enum ZoomState
+    {
+        In,
+        Out
+    }
+    ZoomState zoomState = ZoomState.In;
+
+    public void ZoomButtonClicked()
+    {
+        switch (zoomState)
+        {
+            case ZoomState.In:
+                zoomState = ZoomState.Out;
+                mainCamera.ZoomOut();
+                GetComponentInChildren<Text>().text = "Zoom In";
+                break;
+            case ZoomState.Out:
+                zoomState = ZoomState.In;
+                mainCamera.ZoomIn();
+                GetComponentInChildren<Text>().text = "Zoom Out";
+                break;
+        }
+    }
+
+}
