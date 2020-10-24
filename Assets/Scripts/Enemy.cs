06b63809f5d29513d26d2ac87bf2862ca05c76f5
r7uaz0n <rcktzn007@gmail.com>
2020-10-24 12:50:01 -0700

    Created enemy bot script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 3.0f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+
+    // Start is called before the    first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // lookDirection stores the Vector3 direction that 
+        // the enemy should move towards the the player
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        // MOve the enemy towards the player based off a set speed
+        enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
