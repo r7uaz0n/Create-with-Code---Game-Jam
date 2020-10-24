b289a59c8cc21e9c070d942896c43dec26094315
Justin Chow <jjc2184@caa.columbia.edu>
2020-10-23 17:37:57 -0700

    add axe throwing script

---
+++ Assets/Scripts/AxeThrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeThrow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float throwspeed = 10;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * throwspeed);
+ 
+    }
+}
---
+++ Assets/Scripts/LaunchPickAxe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LaunchPickAxe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject pickaxePrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(pickaxePrefab, transform.position, pickaxePrefab.transform.rotation);
+        }   
+    }
+}
