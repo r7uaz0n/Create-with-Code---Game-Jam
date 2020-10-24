cf2505589f3c618b5dfc1916afd185f0e5db0b6c
Dominic Frei <business.dominic.frei@gmail.com>
2020-10-24 18:57:22 +0100

    Introduce GameState for counting keys.

--- Assets/GameState.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class GameState : MonoBehaviour
-{
-    public static readonly int maximumKeys = 3;
-    public static int keyCounter = 0;
-}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,11 @@
+﻿public class GameState
+{
+    public static int keyCounter = 0;
+
+    private static readonly int maximumKeys = 1;
+
+    public bool isExitRequirementMet()
+    {
+        return keyCounter == maximumKeys;
+    }
+}
