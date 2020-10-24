5f0ea58b6be8318cc8ad842a05fa99bc3e153989
Justin Chow <jjc2184@caa.columbia.edu>
2020-10-23 21:16:33 -0700

    added scripting for pick collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,18 @@
 public class Player : MonoBehaviour
 {
     private float speed = 5.0f;
+    public GameObject pickPrefab;
+    public bool hasPick;
 
+    private void Start()
+    {
+        hasPick = false;
+    }
     void Update()
     {
         UpdateMovement();
+        pickPrefab.SetActive(false);
+        SwingPick();
     }
 
     private void UpdateMovement()
@@ -18,7 +26,25 @@ public class Player : MonoBehaviour
         float deltaZ = inputZ * Time.deltaTime * speed;
 
         Vector3 movement = new Vector3(deltaX, 0.0f, deltaZ);
-
         transform.Translate(movement);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pick"))
+        {
+            pickPrefab.SetActive(true);
+            hasPick = true;
+            Destroy(other.gameObject);
+        }
+
+    }
+
+    private void SwingPick()
+    {
+ 
+    }
+    //pickTransform = transform.Find("Pick");
+    //pickTransform.transform.localPosition = Vector3.Slerp(pickTransform.localPosition, new Vector3(1, 0, 1), 0.01f);
 }
+
