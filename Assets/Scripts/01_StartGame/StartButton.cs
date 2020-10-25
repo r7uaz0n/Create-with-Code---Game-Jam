55f391a49c7820e3e757816e98bd4f8bd58ab4d8
Dominic Frei <business.dominic.frei@gmail.com>
2020-10-25 21:41:58 +0000

    Implement start scene.

---
+++ Assets/Scripts/01_StartGame/StartButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void StartButtonClicked()
+    {
+        SceneManager.LoadScene("02_Map");
+    }
+}
