f9030750c062a962e395b2eb3b01e5477303fb68
r7uaz0n <rcktzn007@gmail.com>
2020-10-24 18:49:00 -0700

    Room 2 update, created enemy prefab with point light

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 0.5f;
+    private float speed = 2.0f;
     private Rigidbody enemyRb;
     private GameObject player;
 
